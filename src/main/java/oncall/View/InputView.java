d2ed9bd5b2cfacb3de6c40c6fa9739962c3e6c44
BurningFalls <sherlocky31415@gmail.com>
2023-12-16 14:37:28 +0900

    fix: 입력받을 때 줄바꿈 없앰

--- src/main/java/oncall/View/InputView.java
+++ src/main/java/oncall/View/InputView.java
@@ -5,17 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String inputMonthDay() {
-        System.out.println("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+        System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
         return Console.readLine();
     }
 
     public static String inputWeekdayWorkers() {
-        System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
         return Console.readLine();
     }
 
     public static String inputWeekendWorkers() {
-        System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        System.out.print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
         return Console.readLine();
     }
 }
