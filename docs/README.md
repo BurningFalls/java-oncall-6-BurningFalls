f0fdc19bb1a19969aa5665eba42bb641b6fa93e3
BurningFalls <sherlocky31415@gmail.com>
2023-12-16 14:02:05 +0900

    feat: MonthDay.isValidInput 구현

--- docs/README.md
+++ docs/README.md
@@ -2,12 +2,15 @@
 
 - [X] oncall 시작하기: OncallController.startOnCall
 - [] 월과 시작 요일 입력 받기: InputView.inputMonthDay
+- [X] 월과 요일 입력이 유효한 형식인지 확인: MonthDay.isValidInput
+- [] 분리했을 때 길이가 2인지 확인: MonthDay.isLengthTwo
+- [] 월과 요일 입력을 콤마 기준으로 분리하기: MonthDay.parseWithComma
 - [] 월이 1부터 12 사이의 숫자인지 확인: MonthDay.isMonth1to12
 - [] 요일이 맞는 형식인지 확인: MonthDay.isDayValidate
 - [] 평일 비상 근무 순서 입력받기: InputView.inputWeekdayWorkers
 - [] 휴일 비상 근무 순서 입력받기: InputView.inputWeekendWorkers
 - [] Workers 입력이 유효한 형식인지 확인: Workers.isValidInput
-- [] Workers 입력을 콤마 기준으로 분리하기: Workers.divideWithComma
+- [] Workers 입력을 콤마 기준으로 분리하기: Workers.parseWithComma
 - [] 근무자의 이름이 최대 5자 이하인지 확인: Workers.isNameUnder5Char
 - [] 근무자의 닉네임이 중복되지 않는지 확인: Workers.isNameDuplicate
 - [] 근무자가 최소 5명 최대 35명인지 확인: Workers.isCount5to35
--- src/main/java/oncall/Model/MonthDay.java
+++ src/main/java/oncall/Model/MonthDay.java
@@ -1,4 +1,36 @@
 package oncall.Model;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class MonthDay {
+    public MonthDay(String input) {
+        isValidInput(input);
+//        List<String> monthDay = parseWithComma(input);
+//        validate(monthDay);
+//
+//        this.monthDay = monthDay;
+    }
+
+    public void isValidInput(String input) {
+        String regex = "^[^,]+(,[^,]+)*$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("[ERROR] 문자열의 형태가 유효하지 않습니다.");
+        }
+    }
+
+//    public List<String> parseWithComma(String input) {
+//        return Arrays.stream(input.split(","))
+//                .map(String::trim)
+//                .filter(str -> !str.isEmpty())
+//                // .map(Integer::parseInt)
+//                .collect(Collectors.toList());
+//    }
 }
